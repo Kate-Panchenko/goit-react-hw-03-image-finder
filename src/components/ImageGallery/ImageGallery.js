75201b5a2d9c803df122c714c44bcca755cdebed
Kate Panchenko <k.panchenko12345@gmail.com>
2023-03-10 22:47:17 +0200

    Gallery done

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -6,47 +6,63 @@ import { Gallery, GalleryImage } from './ImageGallery.styled';
 export class ImageGallery extends Component {
   state = {
     images: null,
-    loading: false,
     error: null,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps, prevState) {
     const prevName = prevProps.searchValue;
     const nextName = this.props.searchValue;
     if (prevName !== nextName) {
-      this.setState({ loading: true, images: null });
+      this.setState({ status: 'pending' });
       getImages(nextName)
-        .then(images => this.setState({ images }))
-        .catch(error => this.setState({ error }))
-        .finally(this.setState({ loading: false }));
+        .then(images => {
+          console.log(images);
+          if (images.total === 0) {
+            return this.setState({
+              error: `No images or photos of ${nextName}`,
+              status: 'rejected',
+            });
+          }
+          this.setState({ images, status: 'resolved' });
+        })
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
 
   render() {
-    const { images, loading, error } = this.state;
+    const { images, error, status } = this.state;
     const { searchValue } = this.props;
-    return (
-      <div>
-        {error && <h2>{error.message}</h2>}
-        {!images && <p>No photos</p>}
-        {loading && <p>Loading...</p>}
-        {images && (
-          <Gallery>
-            {images.hits.map(({ id, webformatURL, largeImageURL }) => {
-              return (
-                <GalleryImage key={id}>
-                  <GalleryItem
-                    webImage={webformatURL}
-                    largeImage={largeImageURL}
-                    id={id}
-                    searchValue={searchValue}
-                  />
-                </GalleryImage>
-              );
-            })}
-          </Gallery>
-        )}
-      </div>
-    );
+
+    if (status === 'idle') {
+      return <p>No photos</p>;
+    }
+
+    if (status === 'pending') {
+      return <p>Loading...</p>;
+    }
+
+    if (status === 'rejected') {
+      return <h2>{error}</h2>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <Gallery>
+          {images.hits.map(({ id, webformatURL, largeImageURL }) => {
+            return (
+              <GalleryImage key={id}>
+                <GalleryItem
+                  webImage={webformatURL}
+                  largeImage={largeImageURL}
+                  id={id}
+                  searchValue={searchValue}
+                />
+              </GalleryImage>
+            );
+          })}
+        </Gallery>
+      );
+    }
   }
 }
